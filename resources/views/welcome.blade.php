10d7217f9d32e4e6dc9e474af15c15cad7b2a7fe
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-08-27 17:37:20 +0300

    Changed page Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen">
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex flex-col justify-center items-center">
-                    <h1 class="font-light text-8xl">Gymn App</h1>
+                    <h1 class="font-light text-8xl">Osego Bookings</h1>
                     <div class="mt-8">
                     @if (Route::has('login'))
                         @auth
